4dafa6826dfb2e7289d43440ab1307d28c72cf68
Brian Riehman <brian@riehman.com>
2017-01-17 08:06:02 -0600

    Update driver to create tables

--- src/main/scala/com/briehman/errorregistry/Driver.scala
+++ src/main/scala/com/briehman/errorregistry/Driver.scala
@@ -3,21 +3,13 @@ package com.briehman.errorregistry
 import com.briehman.errorregistry.models.{AppError, ErrorOccurrence}
 import slick.driver.MySQLDriver.api._
 
-import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-
 object Driver {
   def main(args: Array[String]): Unit = {
     val db = Database.forConfig("mysql")
     try {
-      val a = (AppError.table.schema ++ ErrorOccurrence.table.schema).create
-      val p = db.run(a)
+      val schemas = AppError.table.schema ++ ErrorOccurrence.table.schema
+      val p = db.run(schemas.create)
 
-//      p.foreach { s => println(s) }
-//      Await.ready(db.run(errors.schema.create), Duration.Inf)
-//      val result = errors.result
-//      db.stream(result).foreach(println)
     } catch {
       case e: Exception =>
         Console.err.println(e)
